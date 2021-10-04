e1b696afc15903be916ede5775b389c33f2fac2d
maxagonius <91891795+maxagonius@users.noreply.github.com>
2021-10-04 19:25:37 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,33 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>PHP Store</title>
-	</head>
-	<body>
-		<?php
-			$name = "PHP Store";
-			$credit = 1000;
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
 
-			echo "<h1>Welcome to ".$name."</h1>";
-			echo "<h2>You have $".$credit." in your wallet.</h2>";
+	<?php
+	    $name = "PHP Store";
+			$credit = 1000; 
 
-			$products['Computer']=750;
-			$products['Car']=15000;
-			$products['iPhone']=1000;
-			$products['Toaster']=75;
+	    echo "<h1>Welcome to ".$name."!</h1>";
+	    echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-			foreach($products as $key => $value){
-				echo "<p>The ".$key." costs ".$value."</p>";
-			}
-		?>
-	</body>
+		  $products['Computer']=750;
+	    $products['Car']=15000;
+	    $products['iPhone']=1000;
+	    $products['Toaster']=75;
+
+	    foreach($products as $key => $value){
+		    echo "<p>The ".$key." costs ".$value."</p>";
+	    }
+
+	    echo "<h2>Items you can afford</h2>";
+
+	    foreach($products as $key => $value){
+		    if($value <= $credit ){
+		    	echo "<p>".$key."</p>"; 
+		    }
+	    }
+    ?>
+  </body>
 </html>
