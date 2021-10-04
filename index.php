8a76887970a8a98ed0f5d017f767c0a5cf6000a9
maxagonius <91891795+maxagonius@users.noreply.github.com>
2021-10-04 19:22:46 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP Store</title>
-  </head>
-  <body>
-    <?php
-	$name = "PHP Store";
-	$credit = 1000;
+	<head>
+		<title>PHP Store</title>
+	</head>
+	<body>
+		<?php
+			$name = "PHP Store";
+			$credit = 1000;
 
-	echo "<h1>Welcome to ".$name."!</h1>";
-	echo "<h2>You have $".$credit." in your wallet.</h2>";
+			echo "<h1>Welcome to ".$name."</h1>";
+			echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-		$products['Computer']=750;
-		$products['Car']=15000;
-		$products['iPhone']=1000;
-		$products['Toaster']=75;
+			$products['Computer']=750;
+			$products['Car']=15000;
+			$products['iPhone']=1000;
+			$products['Toaster']=75;
 
-		echo "<p>A car costs $".$products['Car']."</p>";
-    ?>
-  </body>
+			foreach($products as $key => $value){
+				echo "<p>The ".$key." costs ".$value."</p>";
+			}
+		?>
+	</body>
 </html>
